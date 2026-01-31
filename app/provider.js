6c7edfb3d7e3557167eda2b6ad8f81f9ace52330
preetikumari15 <annukumari0928@gmail.com>
2026-01-31 16:13:58 +0530

    new user added to db

--- app/provider.js
+++ app/provider.js
@@ -1,6 +1,31 @@
-import React from 'react'
+"use client";
+import { useUser } from '@clerk/nextjs';
+import { eq } from 'drizzle-orm';
+import React, { useEffect } from 'react'
+import { db } from '@/configs/db';
+import { Users } from '@/configs/schema';
 
 const Provider = ({ children }) => {
+  const {user} = useUser();
+
+  useEffect(() => {
+    user&&isNewUser();
+  }, [user]);
+
+  const isNewUser = async () => {
+    const result = await db.select().from(Users)
+    .where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+
+    if(!result[0]){
+      // Create new user
+      await db.insert(Users).values({
+        email: user?.primaryEmailAddress?.emailAddress,
+        name: user?.fullName,
+        imageUrl: user?.imageUrl
+      });
+    }
+  }
+
   return (
     <div>
         {children}
